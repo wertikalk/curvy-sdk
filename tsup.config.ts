db6d3f41ebef690b1c19c47875177a0494598094
bojinovic <bojinovicc@hotmail.com>
2025-07-21 15:40:21 +0200

    feat: cmt
    cmt

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,25 +1,34 @@
 import { type Options, defineConfig } from "tsup";
 
 export default defineConfig((options) => {
-  const baseConfig: Options = {
-    entry: ["./src/exports/*.[jt]s"],
-    format: ["esm"],
-    target: "es2024",
-    platform: "neutral",
-    treeshake: "recommended",
-    minify: true,
-    bundle: true,
-    splitting: false,
-    esbuildOptions: (options) => {
-      options.assetNames = "[name]";
-      options.loader = {
-        ".wasm": "copy",
-      };
-    },
-  };
+    const baseConfig: Options = {
+        entry: ["./src/exports/*.[jt]s"],
+        format: ["esm"],
+        target: "es2024",
+        platform: "neutral",
+        treeshake: "recommended",
+        minify: true,
+        bundle: true,
+        splitting: false,
+        loader: {
+            ".wasm": "file",
+        },
+        esbuildOptions: (options) => {
+            options.assetNames = "[name]";
+            options.loader = {
+                ".wasm": "copy",
+            };
+        },
+    };
 
-  return [
-    { ...baseConfig, dts: false, clean: true, outDir: "dist/esm" },
-    { ...baseConfig, dts: { only: true }, clean: false, outDir: "dist/types", esbuildOptions: undefined },
-  ];
+    return [
+        { ...baseConfig, dts: false, clean: true, outDir: "dist/esm" },
+        {
+            ...baseConfig,
+            dts: { only: true },
+            clean: false,
+            outDir: "dist/types",
+            esbuildOptions: undefined,
+        },
+    ];
 });
